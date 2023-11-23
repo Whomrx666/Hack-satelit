af67fd789cd14f51681ccd1e7c1643a01f7d034f
Mr.X <145559729+Whomrx666@users.noreply.github.com>
2023-11-23 09:51:30 +0700

    Hack-satelit

---
+++ Hack-satelit.sh
@@ -0,0 +1,178 @@
+#!/usr/bin/bash
+#tools sederhana
+#prank hack satelit
+#by Mr.X
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+echo " "
+echo  $blue "<|=================================|>"
+echo  $red        "|       TOOLS HACK SATELIT          |"
+echo  $red        "|                                   |"
+echo  $red        "| Author        : Mr.X              |"
+echo  $red        "| Contact       : 087855190571      |"
+echo  $red        "| My Team       : BINC              |"
+echo  $red        "| Thanks To     : My self           |"
+echo  $blue "<|=================================|>"
+sleep 1
+echo " "
+echo $green "============== $red MENU $green ================="
+echo $blue "===================================="
+echo $blue "|| $green 1. Geser Satelit Palapa       $blue ||"
+echo $blue "|| $green 2. Geser Satelit Telkom       $blue ||"
+echo $blue "|| $green 3. Geser Satelit Asiasat      $blue ||"
+echo $blue "|| $green 4. Geser Satelit Nasa         $blue ||"
+echo $blue "|| $green 0. Exit The Program           $blue ||"
+echo $blue "===================================="
+echo $blue"╭──[Masukkan Pilihan Anda]>"
+read -p"╰───────root@Whomrx=•>" Gans;
+if [ $Gans = 1 ] || [ $Gans = 01 ]
+then
+echo $blue"╭──[Mau Kearah Mana]>"
+read -p"╰───────root@Whomrx=•>" arah;
+sleep 1
+echo $green"╭──[Mau Digeser Berapa Derajat]>"
+read -p"╰───────root@Whomrx=•>" derajat;
+sleep 1
+echo $red "Loading"
+sleep 1
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "100%"
+sleep 1
+echo $green "Prosessing Done"
+sleep 1
+echo  $blue "Satelit Berhasil Digeser ke arah $arah sejauh $derajat derajat"
+fi
+
+if [ $Gans = 2 ] || [ $Gans = 02 ]
+then
+echo $blue"╭──[Mau Kearah Mana]>"
+read -p"╰───────root@Whomrx=•>" arah;
+sleep 1
+echo $green"╭──[Mau Digeser Berapa Derajat]>"
+read -p"╰───────root@Whomrx=•>" derajat;
+sleep 1
+echo $red "Loading"
+sleep 1
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "100%"
+sleep 1
+echo $green "Prosessing Done"
+sleep 1
+echo  $blue "Satelit Berhasil Digeser ke arah $arah sejauh $derajat derajat"
+fi
+
+if [ $Gans = 3 ] || [ $Gans = 03 ]
+then
+echo $blue"╭──[Mau Kearah Mana]>"
+read -p"╰───────root@Whomrx=•>" arah;
+sleep 1
+echo $green"╭──[Mau Digeser Berapa Derajat]>"
+read -p"╰───────root@Whomrx=•>" derajat;
+sleep 1
+echo $red "Loading"
+sleep 1
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "100%"
+sleep 1
+echo $green "Prosessing Done"
+sleep 1
+echo  $blue "Satelit Berhasil Digeser ke arah $arah sejauh $derajat derajat"
+fi
+
+if [ $Gans = 4 ] || [ $Gans = 04 ]
+then
+echo $blue"╭──[Mau Kearah Mana]>"
+read -p"╰───────root@Whomrx=•>" arah;
+sleep 1
+echo $green"╭──[Mau Digeser Berapa Derajat]>"
+read -p"╰───────root@Whomrx=•>" derajat;
+sleep 1
+echo $red "Loading"
+sleep 1
+echo $blue "10%"
+sleep 1
+echo $green "20%"
+sleep 1
+echo $cyan "30%"
+sleep 1
+echo $purple "40%"
+sleep 1
+echo $blue "50%"
+sleep 1
+echo $red "60%"
+sleep 1
+echo $green "70%"
+sleep 1
+echo $cyan "80%"
+sleep 1
+echo $purple "90%"
+sleep 1
+echo $blue "100%"
+sleep 1
+echo $green "Prosessing Done"
+sleep 1
+echo  $blue "Satelit Berhasil Digeser ke arah $arah sejauh $derajat derajat"
+fi
+
+if [ $Gans = 0 ] || [ $Gans = 00 ]
+then
+echo  $red "BYE BYE"
+echo  $blue "Semoga kalian tidak dikejar fbi"
+echo  $green "Bird intellegent nation cyber"
+exit
+fi
